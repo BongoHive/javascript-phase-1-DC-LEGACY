204293582442dc4e0c27a0136eb1bac579ee3b49
DC <49339350+DC-LEGACY@users.noreply.github.com>
2019-06-11 19:15:35 +0200

    Update variables.js
    Javascript-basics

--- variables.js
+++ variables.js
@@ -1,1 +1,17 @@
-// you solution should be here
+// Question 1
+x = 1994;
+
+y = 2025;
+
+age = y-x
+
+//Question 2
+min = 25
+
+max = 50
+
+a = 2
+
+ total = (max-min)*(2*365)
+
+"You will need "+ total + " to last until the ripe old age of " + max
